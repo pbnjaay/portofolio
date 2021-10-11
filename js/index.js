3ff3e919c4f9884d7b678fb14e41d61871218718
Papa bassirou ndiaye <pbnlgk@gmail.com>
2021-10-11 17:36:57 -0300

    nav link on scroll

--- js/index.js
+++ js/index.js
@@ -20,3 +20,57 @@ const observer = new IntersectionObserver(callback, options);
 
 const target = document.querySelector("#hero");
 observer.observe(target);
+
+let sections = document.querySelectorAll("section");
+
+onscroll = () => {
+  let scrollPosition = document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    // console.log(section.attributes.id.value);
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      let currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+};
+
+let removeAllActiveClasses = () => {
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.classList.remove("text-zenith");
+    el.querySelector("span:nth-of-type(2)").classList.remove("bg-zenith");
+    el.querySelector("span:nth-of-type(2)").classList.remove("w-12");
+  });
+};
+
+let addActiveClass = (id) => {
+  console.log(id);
+  let selector;
+  if (id == "hero") return;
+  selector = `nav a[href="#${id}"]`;
+  el = document.querySelector(selector);
+  el.classList.add("text-zenith");
+  el.querySelector("span:nth-of-type(2)").classList.add("bg-zenith");
+  el.querySelector("span:nth-of-type(2)").classList.add("w-12");
+};
+
+let navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    let currentId = e.target.attributes.href.value;
+    let section = document.querySelector(currentId);
+    let sectionPos = section.offsetTop;
+
+    window.scroll({
+      top: sectionPos,
+      behavior: "smooth",
+    });
+  });
+});
